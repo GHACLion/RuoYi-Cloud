c057ceb22c32b15c97c3cc7bea7cfb7976ba95e5
Lion <hk_lion@yeah.net>
2023-07-03 18:29:53 +0800

    tempressvol 温度/压力/进料量 模块功能

---
+++ src/test/java/com/cssc/srwj/SrwjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cssc.srwj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrwjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
